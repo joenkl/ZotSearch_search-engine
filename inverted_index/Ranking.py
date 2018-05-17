9e5cee17b2fe29ce5dc827bccb45d58c17a1ce50
Unknown <txdao@uci.edu>
2018-05-17 15:05:58 -0700

    Update Ranking score

--- inverted_index/Ranking.py
+++ inverted_index/Ranking.py
@@ -1,11 +1,12 @@
 
-tagDict = { 'title': 7, 
-        'h1': 6, 'h2': 6, 'h3': 6,
-        'h4': 5, 'h5':5, 'h6': 5,
-        'b' : 4,
-        'p': 2}
+tagDict = { 'title': 10, 
+        'h1': 9, 'h2': 8, 'a':8, 'h3': 7,
+        'h4': 6, 'h5':5, 'h6': 5,
+        'b' : 4, 'strong': 4, 'em': 4, 'i': 4, 
+        'p': 2, 'li': 2 }
 
 
+# take only the highest one
 
 
 def tagValue(tag):

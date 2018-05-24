890acbd1fb23e933c589dcf60d98c0d5e8c93846
Unknown <txdao@uci.edu>
2018-05-23 19:19:05 -0700

    temp1

--- search.js
+++ search.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Posting = require("./models/Posting");
 const Loc = require("./models/Location");
-const natural = require('natural');
+const natural = require("natural");
 const tokenizer = new natural.WordTokenizer();
-const stopwords = require('./lib/stopwords')
-const Lemmer = require('lemmer');
-
+const stopwords = require("./lib/stopwords");
+const stemmer = natural.PorterStemmer;
 
 router.post("/", (req, res) => {
   const errors = {};
@@ -16,31 +15,50 @@ router.post("/", (req, res) => {
   var locationList = [];
 
   const resultArray = tokenizer.tokenize(req.body.word.toLowerCase());
-  var resultDict = {};
+  var searchWordTerm = [];
+  var seearhWordPos = [];
+
   resultArray.forEach((item, i) => {
-      
-      if( !stopwords.has(item.trim()) && item.length > 2){
-          
-          resultDict[w] = i;
-      }
+    let term = item.trim();
+    if (!stopwords.has(term) && term.length > 2) {
+      searchWordTerm.push(stemmer.stem(term));
+      seearhWordPos.push(i);
+    }
   });
-  
-  console.log(resultDict);
-
-
-
-
-
-
 
   Posting.find({ wordID: req.body.word.toLowerCase() })
     .limit(30)
+    .populate("Location")
     .then(w => {
       if (!w) {
         errors.word = "There is no URL with this word " + req.body.word;
         return res.status(400).json(errors);
       }
+
       postingList = w;
+
+      for (let item in w) {
+        if (item.post.length > 1) {
+          // w map to dictionary<Term, Position>
+          for()
+          
+          let word1 = []; // dictionary to first term in searchWordTerm
+          let len = searchWordTerm.length;
+          let i = 1;
+          let done = false;
+          while (i < len && !done) {
+            let offset = seearhWordPos[i] - searchWordTerm[i - 1];
+            let word2 = []; // postDic[words]
+
+            word1 = fcoff(word1, word2, offset);
+
+            if (word1.length === 0) {
+              done = true;
+            }
+            i++;
+          }
+        }
+      }
 
       docIDList = [];
       postingList.forEach(p => {

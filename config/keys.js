506ae5e0cac725e7c536f7e68552c1965b7c258b
Joseph Nguyen <josnguyen.kl@gmail.com>
2018-05-24 18:56:25 -0700

    Pretier

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,7 @@
-/*
 module.exports = {
   mongoURI: "mongodb://admin:jnguyen@206.189.76.194:27017/icssearch"
 };
-*/
 
-
-module.exports = {
-  mongoURI: "mongodb://localhost:27017/cs121_stem"
-};
+// module.exports = {
+//   mongoURI: "mongodb://localhost:27017/cs121_stem"
+// };
